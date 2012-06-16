f4a8912978cc4851a19f173a1d3f63a98bbd0d26
terenced <terry.dellino@gmail.com>
2012-06-16 12:46:22 -0400

    handle 'show name - 101 - title'

--- lib/tvparser.rb
+++ lib/tvparser.rb
@@ -6,7 +6,7 @@ module TVParser
     # My Name Is Earl - 3x04.avi
     # MythBusters - S04E01 - Newspaper Crossbow.avi
     # my.name.is.earl.305.hdtv-lol.[VTV].avi
-    
+
     # TODO look up the regex used in XBMC to get data from TV episodes
     re =
       /^(.*?)(?:\s?[-\.]\s?)?\s*\[?s?(?:
@@ -19,15 +19,19 @@ module TVParser
       (?:
         \.(\d{1})(\d{2})(?!\d)
       )
+      |
+      (?:
+        \s*-\s*(\d{1})(\d{2})\s*-\s*(?!\d)
+      )
       )\]?\s?.*$/ix
-    
+
     if match = filename.to_s.match(re)
       series  = match[1].gsub(/[\._]/, ' ').strip.gsub(/\b\w/){$&.upcase}
-      season  = (match[2] || match[5]).to_i
-      episode = (match[3] || match[6]).to_i
+      season  = (match[2] || match[5] || match[7]).to_i
+      episode = (match[3] || match[6] || match[8]).to_i
       episode = (episode)..(match[4].to_i) unless match[4].nil?
-      
-      [series, season, episode] 
+
+      [series, season, episode]
     else
       nil
     end
